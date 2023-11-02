eb77c50204c91fc740279eea9b824f1926688bf9
Jefer Alexis González Romero <104333568+AlexisGR117@users.noreply.github.com>
2023-11-02 14:01:57 -0500

    Update PaintItWebSocketConfig.java

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -2,10 +2,12 @@ package edu.eci.arsw.paintit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
