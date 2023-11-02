24aaf731a5e5da1f1d502ab7b32e14b6afeb16c9
AlexisGR117 <jefer12543@gmail.com>
2023-11-01 22:09:00 -0500

    Modificación del websocket

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -18,9 +18,7 @@ public class PaintItWebSocketConfig implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/stompendpoint").withSockJS();
     }
 
 }
