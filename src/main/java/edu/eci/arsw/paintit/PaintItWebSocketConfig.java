84cab69a39cb937f5309621610f576b8a4d368e3
AlexisGR117 <jefer12543@gmail.com>
2023-11-02 09:36:01 -0500

    Add AntPathMatcher to MessageBrokerRegistry and allow all origins for SockJS endpoint

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -2,6 +2,7 @@ package edu.eci.arsw.paintit;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,13 +13,16 @@ public class PaintItWebSocketConfig implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setPathMatcher(new AntPathMatcher("."));
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint")
+        .setAllowedOrigins("*")
+        .withSockJS();
     }
 
 }
