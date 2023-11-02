c024c0c7ac784ce530c66da08dde1444d65c40ad
AlexisGR117 <jefer12543@gmail.com>
2023-11-02 17:18:51 -0500

    Configuración de websocket

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -3,12 +3,14 @@ package edu.eci.arsw.paintit;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @Configuration
+@CrossOrigin(origins = "*")
 @EnableWebSocketMessageBroker
 public class PaintItWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
