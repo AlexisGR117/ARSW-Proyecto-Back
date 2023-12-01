41819b4a4e62fcbcfb5381a176577f10c325c81f
AlexisGR117 <jefer12543@gmail.com>
2023-12-01 15:47:56 -0500

    Prueba de websocket

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -22,7 +22,7 @@ public class PaintItWebSocketConfig implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stompendpoint")
-                .setAllowedOrigins("https://paintitapp.azurewebsites.net")
+                .setAllowedOrigins("*")
                 .withSockJS();
 
     }
