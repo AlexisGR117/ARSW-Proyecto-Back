773eb82adce5431ea9f46d81c3f166e5b8c65ead
AlexisGR117 <jefer12543@gmail.com>
2023-12-04 14:05:55 -0500

    Permitir origen de http

--- src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/paintit/PaintItWebSocketConfig.java
@@ -22,7 +22,12 @@ public class PaintItWebSocketConfig implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stompendpoint")
-                .setAllowedOrigins("https://paintitapp.azurewebsites.net", "https://paintitfront.azurewebsites.net", "https://paintitgateway.eastus.cloudapp.azure.com")
+                .setAllowedOrigins("https://paintitapp.azurewebsites.net",
+                                "https://paintitfront.azurewebsites.net",
+                                "https://paintitgateway.eastus.cloudapp.azure.com",
+                                "http://paintitapp.azurewebsites.net",
+                                "http://paintitfront.azurewebsites.net",
+                                "http://paintitgateway.eastus.cloudapp.azure.com")
                 .withSockJS();
     }
 
